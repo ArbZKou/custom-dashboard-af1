e928a90ccb27792afbe024253bb49fea7f3a3eb8
zhekou <zkou@arb.com.au>
2026-06-29 11:20:38 +1000

    update

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-# import streamlit as st
-
-# st.title("Hello Streamlit")
-# st.write("First Streamlit App - Axis Freq")
-
 import streamlit as st
+
+st.set_page_config(page_title="Custom Dashboard", layout="wide")
 
 st.title("🏠 Home")
 
 st.write("欢迎进入多页面 Streamlit 应用")
 
-st.sidebar.success("请从左侧选择页面 👈")
+st.sidebar.success("请从左侧选择页面 👉")
